3c672662fdd5a8a9ca22329aabfbea82fd0aa1a9
Max Reitz <xanclic@xanclic.moe>
2018-04-24 19:31:35 +0200

    Flag and unveil all surrounding fields
    If the number of flags in a safe field's environment is equal to its
    label, unveil all of them when clicked.
    
    If the number of potential mines in a safe field's environment is equal
    to its label, flag all of them.
    
    This allows you to kind of play the game now.

--- src/logic.rs
+++ src/logic.rs
@@ -2,6 +2,7 @@ use game::{FieldLabel, Game};
 use gui::GUI;
 
 
+#[derive(PartialEq, Clone, Copy)]
 pub enum FieldState {
     Veiled,
     Flagged,
@@ -14,28 +15,28 @@ pub struct Logic {
     game: Game,
 
     mines_spread: bool,
-    unveiled: Vec<Vec<bool>>,
+    game_state: Vec<Vec<FieldState>>,
 }
 
 
 impl Logic {
     pub fn new(game: Game) -> Self {
         let dim = game.get_dim();
-        let mut unveiled_vec = Vec::<Vec<bool>>::new();
+        let mut state_vec = Vec::<Vec<FieldState>>::new();
 
         for _ in 0..dim.1 {
-            let mut row = Vec::<bool>::new();
+            let mut row = Vec::<FieldState>::new();
             for _ in 0..dim.0 {
-                row.push(false);
+                row.push(FieldState::Veiled);
             }
-            unveiled_vec.push(row);
+            state_vec.push(row);
         }
 
         Logic {
             game: game,
 
             mines_spread: false,
-            unveiled: unveiled_vec,
+            game_state: state_vec,
         }
     }
 
@@ -43,21 +44,46 @@ impl Logic {
         self.game.get_dim()
     }
 
-    fn unveil_signed(&mut self, gui: &mut GUI, pos: (i32, i32)) {
+    fn pos_in_bounds(&self, pos: (i32, i32)) -> Option<(usize, usize)> {
         let dim = self.game.get_dim();
 
         if pos.0 >= 0 && (pos.0 as usize) < dim.0 &&
            pos.1 >= 0 && (pos.1 as usize) < dim.1
         {
-            self.unveil(gui, (pos.0 as usize, pos.1 as usize));
+            Some((pos.0 as usize, pos.1 as usize))
+        } else {
+            None
+        }
+    }
+
+    fn unveil_surrounding(&mut self, gui: &mut GUI, pos: (usize, usize)) {
+        let ipos = (pos.0 as i32, pos.1 as i32);
+        for yd in -1..2 {
+            for xd in -1..2 {
+                let dpos = (ipos.0 + xd, ipos.1 + yd);
+                if let Some(upos) = self.pos_in_bounds(dpos) {
+                    self.unveil(gui, upos);
+                }
+            }
+        }
+    }
+
+    fn flag_surrounding(&mut self, gui: &mut GUI, pos: (usize, usize)) {
+        let ipos = (pos.0 as i32, pos.1 as i32);
+        for yd in -1..2 {
+            for xd in -1..2 {
+                let dpos = (ipos.0 + xd, ipos.1 + yd);
+                if let Some(upos) = self.pos_in_bounds(dpos) {
+                    self.flag(gui, upos);
+                }
+            }
         }
     }
 
     fn unveil(&mut self, gui: &mut GUI, pos: (usize, usize)) {
-        if self.unveiled[pos.1][pos.0] {
+        if self.game_state[pos.1][pos.0] != FieldState::Veiled {
             return;
         }
-        self.unveiled[pos.1][pos.0] = true;
 
         if !self.mines_spread {
             self.game.spread_mines(pos);
@@ -65,6 +91,12 @@ impl Logic {
         }
 
         let label = self.game.get_field_label(pos);
+
+        let state = match label {
+            FieldLabel::Mine    => FieldState::Mine,
+            FieldLabel::Safe(n) => FieldState::Safe(n),
+        };
+        self.game_state[pos.1][pos.0] = state;
 
         match label {
             FieldLabel::Mine => {
@@ -84,27 +116,105 @@ impl Logic {
             },
 
             FieldLabel::Safe(0) => {
-                /* Unveil surrounding area */
-                let ipos = (pos.0 as i32, pos.1 as i32);
-                for yd in -1..2 {
-                    for xd in -1..2 {
-                        self.unveil_signed(gui, (ipos.0 + xd, ipos.1 + yd));
-                    }
-                }
+                self.unveil_surrounding(gui, pos);
             },
 
             FieldLabel::Safe(_) => (),
         }
 
-        let state = match label {
-            FieldLabel::Mine    => FieldState::Mine,
-            FieldLabel::Safe(n) => FieldState::Safe(n),
-        };
-
         gui.set_field_state(pos, state);
     }
 
+    fn flag(&mut self, gui: &mut GUI, pos: (usize, usize)) {
+        if self.game_state[pos.1][pos.0] != FieldState::Veiled {
+            return;
+        }
+
+        self.game_state[pos.1][pos.0] = FieldState::Flagged;
+        gui.set_field_state(pos, FieldState::Flagged);
+    }
+
+    fn unflag(&mut self, gui: &mut GUI, pos: (usize, usize)) {
+        if self.game_state[pos.1][pos.0] != FieldState::Flagged {
+            return;
+        }
+
+        self.game_state[pos.1][pos.0] = FieldState::Veiled;
+        gui.set_field_state(pos, FieldState::Veiled);
+    }
+
+    fn definitely_mined(&self, _pos: (usize, usize)) -> bool {
+        false
+    }
+
+    fn get_state(&self, pos: (i32, i32)) -> Option<FieldState> {
+        if let Some(upos) = self.pos_in_bounds(pos) {
+            Some(self.game_state[upos.1][upos.0])
+        } else {
+            None
+        }
+    }
+
+    fn unveil_surrounding_if_safe(&mut self, gui: &mut GUI, pos: (usize, usize))
+    {
+        let n;
+        match self.game_state[pos.1][pos.0] {
+            FieldState::Safe(x) => n = x,
+
+            _ => return
+        };
+
+        let mut flag_count = 0;
+        let mut potential_mine_count = 0;
+        let ipos = (pos.0 as i32, pos.1 as i32);
+        for yd in -1..2 {
+            for xd in -1..2 {
+                let state = self.get_state((ipos.0 + xd, ipos.1 + yd));
+                match state {
+                    Some(FieldState::Veiled) => {
+                        potential_mine_count += 1;
+                    }
+
+                    Some(FieldState::Flagged) => {
+                        flag_count += 1;
+                        potential_mine_count += 1;
+                    },
+
+                    Some(FieldState::Mine) => {
+                        potential_mine_count += 1;
+                    },
+
+                    _ => ()
+                };
+            }
+        }
+
+        if flag_count == n {
+            self.unveil_surrounding(gui, pos);
+        } else if potential_mine_count == n {
+            self.flag_surrounding(gui, pos);
+        }
+    }
+
     pub fn pressed(&mut self, gui: &mut GUI, pos: (usize, usize)) {
-        self.unveil(gui, pos);
+        match self.game_state[pos.1][pos.0] {
+            FieldState::Veiled => {
+                if self.definitely_mined(pos) {
+                    self.flag(gui, pos);
+                } else {
+                    self.unveil(gui, pos);
+                }
+            },
+
+            FieldState::Flagged => {
+                self.unflag(gui, pos);
+            },
+
+            FieldState::Mine => (),
+
+            FieldState::Safe(_) => {
+                self.unveil_surrounding_if_safe(gui, pos);
+            },
+        }
     }
 }
