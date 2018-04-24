377f23cdba41516d71eeb3342b251d1083a262bb
Hanna Reitz <xanclic@xanclic.moe>
2018-04-24 18:51:13 +0200

    Make field size configurable
    The field size (and mine count) can now be configured through the
    command line.

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,22 @@ use logic::Logic;
 
 
 fn main() {
-    let game = Game::new((9, 9), 10);
+    let args: Vec<String> = std::env::args().collect();
+
+    let width;
+    let height;
+    let mine_count;
+    if args.len() <= 1 {
+        width = 30;
+        height = 16;
+        mine_count = 99;
+    } else {
+        width = args[1].parse::<usize>().unwrap();
+        height = args[2].parse::<usize>().unwrap();
+        mine_count = args[3].parse::<usize>().unwrap();
+    }
+
+    let game = Game::new((width, height), mine_count);
     let logic = Logic::new(game);
     let gui = GUI::new(logic);
 
